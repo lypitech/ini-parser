f857e76a0e31676926094da4518475b7bc508ff8
Lysandre B. <lysandre.boursette@epitech.eu>
2025-11-19 18:05:58 +0100

    feat(tests): Added sample test.

--- tests/test_example.cpp
+++ tests/test_example.cpp
@@ -1,9 +1,16 @@
 #include <gtest/gtest.h>
 
+#include "yml/Yml.h"
+
 TEST(SampleTest, Addition) {
     EXPECT_EQ(1 + 1, 2);
 }
 
 TEST(SampleTest, FalseTest) {
     EXPECT_FALSE(false);
+}
+
+TEST(Basic, StringGet) {
+    yml::Yml yml("../../tests/yml/1.yml");
+    EXPECT_EQ(yml["hello"].as<std::string>(), "world");
 }
